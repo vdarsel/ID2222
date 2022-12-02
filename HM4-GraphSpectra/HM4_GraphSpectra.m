5992b67d026ec4f21fe1f72708123005b5540733
Vianey Darsel <vdarsel@live.fr>
2022-12-02 21:21:50 +0100

    Update of graphs/add of the report

--- HM4-GraphSpectra/HM4_GraphSpectra.m
+++ HM4-GraphSpectra/HM4_GraphSpectra.m
@@ -1,4 +1,4 @@
-function p_1 = HM4_GraphSpectra(file_numb)
+function HM4_GraphSpectra(file_numb)
     currentFile = mfilename( 'fullpath' );
     [pathstr,~,~] = fileparts( currentFile );
     addpath( fullfile( pathstr, '\data\' ) );
@@ -11,7 +11,7 @@ function p_1 = HM4_GraphSpectra(file_numb)
     As= sparse(col1, col2, 1, max_ids, max_ids); 
     A = full(As);
     A = min(A,1); %remove value over 1
-    D = diag(sum(A));
+    %D = diag(sum(A));
     c= sum(A);
     disp(min(c)+">0, so D is inversible");
     inv_c = 1./c;
@@ -21,7 +21,7 @@ function p_1 = HM4_GraphSpectra(file_numb)
     eigen_values = diag(DiagL);
     %already sort sorted_eigenvalues = sort(eigen_values,1,"descend");
     delta = eigen_values(2:max_ids,1)-eigen_values(1:max_ids-1,1);
-    [val, n_clusters] = max(delta);
+    [~, n_clusters] = max(delta);
     n_clusters = max_ids -n_clusters;
     disp("The number of clusters is "+n_clusters+".");
     X = v(1:max_ids,max_ids-n_clusters+1:max_ids);
@@ -30,23 +30,38 @@ function p_1 = HM4_GraphSpectra(file_numb)
     for i=1:n_clusters
         Y(1:max_ids,i)=Y(1:max_ids,i)./row_X(1:max_ids,1);
     end
-    idx = kmeans(Y,n_clusters);
+    clust = kmeans(Y,n_clusters);
+    %plot parameters
     G = graph(A,'omitselfloops');
-    t=2*pi()*(1:1:n_clusters)/n_clusters;
+    t=2*pi()*(0.5:1:n_clusters-0.5)/n_clusters;
     n=2*pi()*(1:1:max_ids)/max_ids;
     R = 5;
     r=1;
-    cos_t = arrayfun(@(var) cos(var),t);
-    [~, order] = sort(idx);
-    Index = zeros(size(idx));
+    [~, order] = sort(clust);
+    Index = zeros(size(clust));
     for i=1:max_ids
         Index(i) = find(order==i);
     end
-    tiledlayout(1,3)
+    t_plot = tiledlayout(1,3);
     nexttile
-    p_3 = scatter(col1,col2, 6,"filled");
+    scatter(col1,col2, 6,"filled");
+    title('Original Sparsity Pattern');
     nexttile
-    p_1 = plot(G,'XData',R*arrayfun(@(var) cos(var),t(idx))+r*arrayfun(@(var) cos(var),n),'YData',R*arrayfun(@(var) sin(var),t(idx))+r*arrayfun(@(var) sin(var),n),'Marker','.');
+    plot(G,'XData',R*arrayfun(@(var) cos(var),t(clust))+r*arrayfun(@(var) cos(var),n),'YData',R*arrayfun(@(var) sin(var),t(clust))+r*arrayfun(@(var) sin(var),n),'Marker','.','NodeLabel',{});
+    title('Graph after computing the communities');
     nexttile
-    p_2 = scatter(Index(col1),Index(col2), 6,"filled");
+    scatter(Index(col1),Index(col2), 6,"filled");
+    title('Reordered Sparsity Pattern');
+    title(t_plot,['Example ' int2str(file_numb)]);
+    figure;
+    t_plot_2 = tiledlayout(1,n_clusters);
+    for i=1:n_clusters
+        nexttile;
+        plot(Y(order(:),i));
+        title(['x' int2str(i)]);
+        ylim([-1.2,1.2]);
+    end
+    title(t_plot_2,['Example ' int2str(file_numb) ': Component value']);
 end
+
+
